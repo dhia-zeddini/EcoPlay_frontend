bc7cabb6f0143a4b67471e29ed65529960823af6
ramilaribi <laribi.rami@esprit.tn>
2023-11-06 23:52:40 +0100

    product detail ui

---
+++ app/src/main/java/com/example/ecoplay_frontend/view/details.kt
@@ -0,0 +1,12 @@
+package com.example.ecoplay_frontend.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecoplay_frontend.R
+
+class details : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
