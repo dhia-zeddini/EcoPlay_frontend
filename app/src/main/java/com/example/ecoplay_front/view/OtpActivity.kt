b57661ddc1c038d6e7d9a221605cef9d455b78e3
dhia-zeddini <dhia.zeddini.14@gmail.com>
2023-11-06 22:46:15 +0100

    otp ui

---
+++ app/src/main/java/com/example/ecoplay_front/view/OtpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecoplay_front.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecoplay_front.R
+
+class OtpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+    }
+}
